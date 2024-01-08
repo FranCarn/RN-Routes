7b62597a7345523644fb977786c9406bc237b70e
@FranCarn <carnevalefranco99@gmail.com>
2024-01-08 15:17:44 -0300

    context skeleton ready

--- App.tsx
+++ App.tsx
@@ -2,11 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {MainNavigator} from './src/navigator/Navigator';
+import {PermissionsProvider} from './src/context/PermissionsContext';
+
+const AppState = ({children}: {children: JSX.Element | JSX.Element[]}) => {
+  return <PermissionsProvider>{children}</PermissionsProvider>;
+};
 
 export default function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <MainNavigator />
+      <AppState>
+        <MainNavigator />
+      </AppState>
     </NavigationContainer>
   );
 }
---
+++ src/context/PermissionsContext.tsx
@@ -0,0 +1,42 @@
+import React, {createContext, useState} from 'react';
+import {PermissionStatus} from 'react-native-permissions';
+
+export interface PermissionsState {
+  locationStatus: PermissionStatus;
+}
+
+export const permissionInitState: PermissionsState = {
+  locationStatus: 'unavailable',
+};
+
+type PermissionsContextProps = {
+  permissions: PermissionsState;
+  askLocationPermission: () => void;
+  checkLocationPermission: () => void;
+};
+
+export const PermissionsContext = createContext({} as PermissionsContextProps);
+
+export const PermissionsProvider = ({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) => {
+  const [permissions, setPermissions] =
+    useState<PermissionsState>(permissionInitState);
+
+  const askLocationPermission = () => {};
+
+  const checkLocationPermission = () => {};
+
+  return (
+    <PermissionsContext.Provider
+      value={{
+        permissions,
+        askLocationPermission,
+        checkLocationPermission,
+      }}>
+      {children}
+    </PermissionsContext.Provider>
+  );
+};
