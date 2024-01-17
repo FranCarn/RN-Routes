692c466546eb7e7375fce46ecb08db2b75d0fb3c
@FranCarn <carnevalefranco99@gmail.com>
2024-01-17 16:55:25 -0300

    map component escalable ready

---
+++ src/components/Map.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View} from 'react-native';
+import MapView, {
+  MapMarkerProps,
+  Marker,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
+
+interface Props {
+  markers?: MapMarkerProps[];
+}
+
+export const Map = ({markers}: Props) => {
+  return (
+    <>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{flex: 1}}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        {markers?.map(item => (
+          <Marker key={item.title} {...item} />
+        ))}
+      </MapView>
+    </>
+  );
+};
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {View} from 'react-native';
+import {Map} from '../components/Map';
 
 export const MapScreen = () => {
   return (
@@ -12,24 +12,7 @@ export const MapScreen = () => {
         alignItems: 'center',
         justifyContent: 'center',
       }}>
-      <Text>Map Screen</Text>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 100,
-        }}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={{flex: 1}}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        />
-      </View>
+      <Map />
     </View>
   );
 };
